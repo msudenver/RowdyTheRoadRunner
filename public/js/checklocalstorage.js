f1fe284dbea77f1777b7fcfd3c4735a274a1e487
garzawi@msudenver.edu <garzawi@msudenver.edu>
2013-06-11 10:01:51 -0600

    adds all new bootstrap js files and index.html updates

---
+++ public/js/checklocalstorage.js
@@ -0,0 +1,8 @@
+// modernizr check
+// for localStorage support
+
+// if (Modernizr.localstorage){
+// 	log("localStorage is available");
+// }else{
+// 	log("No native support for localStorage, load dojox.storage solution ...");
+// }
