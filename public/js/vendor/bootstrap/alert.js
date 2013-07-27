5c486dbbdd88f7cae742188ba2ccc9dd16a57556
Miguel Garza Wicker <garzawi@msudenver.edu>
2013-07-27 14:44:01 -0600

    Adds alert and modal js BS3 RC1

--- public/js/vendor/bootstrap/alert.js
+++ public/js/vendor/bootstrap/alert.js
@@ -1,62 +1,98 @@
-$(function () {
+/* ========================================================================
+ * Bootstrap: alert.js v3.0.0
+ * http://twbs.github.com/bootstrap/javascript.html#alerts
+ * ========================================================================
+ * Copyright 2013 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ======================================================================== */
 
-    module("alert")
 
-      test("should provide no conflict", function () {
-        var alert = $.fn.alert.noConflict()
-        ok(!$.fn.alert, 'alert was set back to undefined (org value)')
-        $.fn.alert = alert
-      })
++function ($) { "use strict";
 
-      test("should be defined on jquery object", function () {
-        ok($(document.body).alert, 'alert method is defined')
-      })
+  // ALERT CLASS DEFINITION
+  // ======================
 
-      test("should return element", function () {
-        ok($(document.body).alert()[0] == document.body, 'document.body returned')
-      })
+  var dismiss = '[data-dismiss="alert"]'
+  var Alert   = function (el) {
+    $(el).on('click', dismiss, this.close)
+  }
 
-      test("should fade element out on clicking .close", function () {
-        var alertHTML = '<div class="alert-message warning fade in">'
-          + '<a class="close" href="#" data-dismiss="alert">×</a>'
-          + '<p><strong>Holy guacamole!</strong> Best check yo self, you\'re not looking too good.</p>'
-          + '</div>'
-          , alert = $(alertHTML).alert()
+  Alert.prototype.close = function (e) {
+    var $this    = $(this)
+    var selector = $this.attr('data-target')
 
-        alert.find('.close').click()
+    if (!selector) {
+      selector = $this.attr('href')
+      selector = selector && selector.replace(/.*(?=#[^\s]*$)/, '') // strip for ie7
+    }
 
-        ok(!alert.hasClass('in'), 'remove .in class on .close click')
-      })
+    var $parent = $(selector)
 
-      test("should remove element when clicking .close", function () {
-        $.support.transition = false
+    if (e) e.preventDefault()
 
-        var alertHTML = '<div class="alert-message warning fade in">'
-          + '<a class="close" href="#" data-dismiss="alert">×</a>'
-          + '<p><strong>Holy guacamole!</strong> Best check yo self, you\'re not looking too good.</p>'
-          + '</div>'
-          , alert = $(alertHTML).appendTo('#qunit-fixture').alert()
+    if (!$parent.length) {
+      $parent = $this.hasClass('alert') ? $this : $this.parent()
+    }
 
-        ok($('#qunit-fixture').find('.alert-message').length, 'element added to dom')
+    $parent.trigger(e = $.Event('close.bs.alert'))
 
-        alert.find('.close').click()
+    if (e.isDefaultPrevented()) return
 
-        ok(!$('#qunit-fixture').find('.alert-message').length, 'element removed from dom')
-      })
+    $parent.removeClass('in')
 
-      test("should not fire closed when close is prevented", function () {
-        $.support.transition = false
-        stop();
-        $('<div class="alert"/>')
-          .on('close.bs.alert', function (e) {
-            e.preventDefault();
-            ok(true);
-            start();
-          })
-          .on('closed.bs.alert', function () {
-            ok(false);
-          })
-          .alert('close')
-      })
+    function removeElement() {
+      $parent.trigger('closed.bs.alert').remove()
+    }
 
-})
+    $.support.transition && $parent.hasClass('fade') ?
+      $parent
+        .one($.support.transition.end, removeElement)
+        .emulateTransitionEnd(150) :
+      removeElement()
+  }
+
+
+  // ALERT PLUGIN DEFINITION
+  // =======================
+
+  var old = $.fn.alert
+
+  $.fn.alert = function (option) {
+    return this.each(function () {
+      var $this = $(this)
+      var data  = $this.data('bs.alert')
+
+      if (!data) $this.data('bs.alert', (data = new Alert(this)))
+      if (typeof option == 'string') data[option].call($this)
+    })
+  }
+
+  $.fn.alert.Constructor = Alert
+
+
+  // ALERT NO CONFLICT
+  // =================
+
+  $.fn.alert.noConflict = function () {
+    $.fn.alert = old
+    return this
+  }
+
+
+  // ALERT DATA-API
+  // ==============
+
+  $(document).on('click.bs.alert.data-api', dismiss, Alert.prototype.close)
+
+}(window.jQuery);
--- public/js/vendor/bootstrap/modal.js
+++ public/js/vendor/bootstrap/modal.js
@@ -1,156 +1,241 @@
-$(function () {
+/* ========================================================================
+ * Bootstrap: modal.js v3.0.0
+ * http://twbs.github.com/bootstrap/javascript.html#modals
+ * ========================================================================
+ * Copyright 2012 Twitter, Inc.
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ * ======================================================================== */
 
-    module("modal")
 
-      test("should provide no conflict", function () {
-        var modal = $.fn.modal.noConflict()
-        ok(!$.fn.modal, 'modal was set back to undefined (org value)')
-        $.fn.modal = modal
++function ($) { "use strict";
+
+  // MODAL CLASS DEFINITION
+  // ======================
+
+  var Modal = function (element, options) {
+    this.options   = options
+    this.$element  = $(element).on('click.dismiss.modal', '[data-dismiss="modal"]', $.proxy(this.hide, this))
+    this.$backdrop =
+    this.isShown   = null
+
+    if (this.options.remote) this.$element.find('.modal-body').load(this.options.remote)
+  }
+
+  Modal.DEFAULTS = {
+      backdrop: true
+    , keyboard: true
+    , show: true
+  }
+
+  Modal.prototype.toggle = function () {
+    return this[!this.isShown ? 'show' : 'hide']()
+  }
+
+  Modal.prototype.show = function () {
+    var that = this
+    var e    = $.Event('show.bs.modal')
+
+    this.$element.trigger(e)
+
+    if (this.isShown || e.isDefaultPrevented()) return
+
+    this.isShown = true
+
+    this.escape()
+
+    this.backdrop(function () {
+      var transition = $.support.transition && that.$element.hasClass('fade')
+
+      if (!that.$element.parent().length) {
+        that.$element.appendTo(document.body) // don't move modals dom position
+      }
+
+      that.$element.show()
+
+      if (transition) {
+        that.$element[0].offsetWidth // force reflow
+      }
+
+      that.$element
+        .addClass('in')
+        .attr('aria-hidden', false)
+
+      that.enforceFocus()
+
+      transition ?
+        that.$element
+          .one($.support.transition.end, function () {
+            that.$element.focus().trigger('shown.bs.modal')
+          })
+          .emulateTransitionEnd(300) :
+        that.$element.focus().trigger('shown.bs.modal')
+    })
+  }
+
+  Modal.prototype.hide = function (e) {
+    if (e) e.preventDefault()
+
+    e = $.Event('hide.bs.modal')
+
+    this.$element.trigger(e)
+
+    if (!this.isShown || e.isDefaultPrevented()) return
+
+    this.isShown = false
+
+    this.escape()
+
+    $(document).off('focusin.bs.modal')
+
+    this.$element
+      .removeClass('in')
+      .attr('aria-hidden', true)
+
+    $.support.transition && this.$element.hasClass('fade') ?
+      this.$element
+        .one($.support.transition.end, $.proxy(this.hideModal, this))
+        .emulateTransitionEnd(300) :
+      this.hideModal()
+  }
+
+  Modal.prototype.enforceFocus = function () {
+    $(document)
+      .off('focusin.bs.modal') // guard against infinite focus loop
+      .on('focusin.bs.modal', $.proxy(function (e) {
+        if (this.$element[0] !== e.target && !this.$element.has(e.target).length) {
+          this.$element.focus()
+        }
+      }, this))
+  }
+
+  Modal.prototype.escape = function () {
+    if (this.isShown && this.options.keyboard) {
+      this.$element.on('keyup.dismiss.bs.modal', $.proxy(function (e) {
+        e.which == 27 && this.hide()
+      }, this))
+    } else if (!this.isShown) {
+      this.$element.off('keyup.dismiss.bs.modal')
+    }
+  }
+
+  Modal.prototype.hideModal = function () {
+    var that = this
+    this.$element.hide()
+    this.backdrop(function () {
+      that.removeBackdrop()
+      that.$element.trigger('hidden.bs.modal')
+    })
+  }
+
+  Modal.prototype.removeBackdrop = function () {
+    this.$backdrop && this.$backdrop.remove()
+    this.$backdrop = null
+  }
+
+  Modal.prototype.backdrop = function (callback) {
+    var that    = this
+    var animate = this.$element.hasClass('fade') ? 'fade' : ''
+
+    if (this.isShown && this.options.backdrop) {
+      var doAnimate = $.support.transition && animate
+
+      this.$backdrop = $('<div class="modal-backdrop ' + animate + '" />')
+        .appendTo(document.body)
+
+      this.$element.on('click', $.proxy(function (e) {
+        if (e.target !== e.currentTarget) return
+        this.options.backdrop == 'static'
+          ? this.$element[0].focus.call(this.$element[0])
+          : this.hide.call(this)
+      }, this))
+
+      if (doAnimate) this.$backdrop[0].offsetWidth // force reflow
+
+      this.$backdrop.addClass('in')
+
+      if (!callback) return
+
+      doAnimate ?
+        this.$backdrop
+          .one($.support.transition.end, callback)
+          .emulateTransitionEnd(150) :
+        callback()
+
+    } else if (!this.isShown && this.$backdrop) {
+      this.$backdrop.removeClass('in')
+
+      $.support.transition && this.$element.hasClass('fade')?
+        this.$backdrop
+          .one($.support.transition.end, callback)
+          .emulateTransitionEnd(150) :
+        callback()
+
+    } else if (callback) {
+      callback()
+    }
+  }
+
+
+  // MODAL PLUGIN DEFINITION
+  // =======================
+
+  var old = $.fn.modal
+
+  $.fn.modal = function (option) {
+    return this.each(function () {
+      var $this   = $(this)
+      var data    = $this.data('bs.modal')
+      var options = $.extend({}, Modal.DEFAULTS, $this.data(), typeof option == 'object' && option)
+
+      if (!data) $this.data('bs.modal', (data = new Modal(this, options)))
+      if (typeof option == 'string') data[option]()
+      else if (options.show) data.show()
+    })
+  }
+
+  $.fn.modal.Constructor = Modal
+
+
+  // MODAL NO CONFLICT
+  // =================
+
+  $.fn.modal.noConflict = function () {
+    $.fn.modal = old
+    return this
+  }
+
+
+  // MODAL DATA-API
+  // ==============
+
+  $(document).on('click.bs.modal.data-api', '[data-toggle="modal"]', function (e) {
+    var $this   = $(this)
+    var href    = $this.attr('href')
+    var $target = $($this.attr('data-target') || (href && href.replace(/.*(?=#[^\s]+$)/, ''))) //strip for ie7
+    var option  = $target.data('modal') ? 'toggle' : $.extend({ remote:!/#/.test(href) && href }, $target.data(), $this.data())
+
+    e.preventDefault()
+
+    $target
+      .modal(option)
+      .one('hide', function () {
+        $this.is(':visible') && $this.focus()
       })
+  })
 
-      test("should be defined on jquery object", function () {
-        var div = $("<div id='modal-test'></div>")
-        ok(div.modal, 'modal method is defined')
-      })
+  var $body = $(document.body)
+    .on('shown.bs.modal',  '.modal', function () { $body.addClass('modal-open') })
+    .on('hidden.bs.modal', '.modal', function () { $body.removeClass('modal-open') })
 
-      test("should return element", function () {
-        var div = $("<div id='modal-test'></div>")
-        ok(div.modal() == div, 'document.body returned')
-        $('#modal-test').remove()
-      })
-
-      test("should expose defaults var for settings", function () {
-        ok($.fn.modal.Constructor.DEFAULTS, 'default object exposed')
-      })
-
-      test("should insert into dom when show method is called", function () {
-        stop()
-        $.support.transition = false
-        $("<div id='modal-test'></div>")
-          .on("shown.bs.modal", function () {
-            ok($('#modal-test').length, 'modal inserted into dom')
-            $(this).remove()
-            start()
-          })
-          .modal("show")
-      })
-
-      test("should fire show event", function () {
-        stop()
-        $.support.transition = false
-        $("<div id='modal-test'></div>")
-          .on("show.bs.modal", function () {
-            ok(true, "show was called")
-          })
-          .on("shown.bs.modal", function () {
-            $(this).remove()
-            start()
-          })
-          .modal("show")
-      })
-
-      test("should not fire shown when default prevented", function () {
-        stop()
-        $.support.transition = false
-        $("<div id='modal-test'></div>")
-          .on("show.bs.modal", function (e) {
-            e.preventDefault()
-            ok(true, "show was called")
-            start()
-          })
-          .on("shown.bs.modal", function () {
-            ok(false, "shown was called")
-          })
-          .modal("show")
-      })
-
-      test("should hide modal when hide is called", function () {
-        stop()
-        $.support.transition = false
-
-        $("<div id='modal-test'></div>")
-          .on("shown.bs.modal", function () {
-            ok($('#modal-test').is(":visible"), 'modal visible')
-            ok($('#modal-test').length, 'modal inserted into dom')
-            $(this).modal("hide")
-          })
-          .on("hidden.bs.modal", function() {
-            ok(!$('#modal-test').is(":visible"), 'modal hidden')
-            $('#modal-test').remove()
-            start()
-          })
-          .modal("show")
-      })
-
-      test("should toggle when toggle is called", function () {
-        stop()
-        $.support.transition = false
-        var div = $("<div id='modal-test'></div>")
-        div
-          .on("shown.bs.modal", function () {
-            ok($('#modal-test').is(":visible"), 'modal visible')
-            ok($('#modal-test').length, 'modal inserted into dom')
-            div.modal("toggle")
-          })
-          .on("hidden.bs.modal", function() {
-            ok(!$('#modal-test').is(":visible"), 'modal hidden')
-            div.remove()
-            start()
-          })
-          .modal("toggle")
-      })
-
-      test("should remove from dom when click [data-dismiss=modal]", function () {
-        stop()
-        $.support.transition = false
-        var div = $("<div id='modal-test'><span class='close' data-dismiss='modal'></span></div>")
-        div
-          .on("shown.bs.modal", function () {
-            ok($('#modal-test').is(":visible"), 'modal visible')
-            ok($('#modal-test').length, 'modal inserted into dom')
-            div.find('.close').click()
-          })
-          .on("hidden.bs.modal", function() {
-            ok(!$('#modal-test').is(":visible"), 'modal hidden')
-            div.remove()
-            start()
-          })
-          .modal("toggle")
-      })
-
-      test("should allow modal close with 'backdrop:false'", function () {
-        stop()
-        $.support.transition = false
-        var div = $("<div>", { id: 'modal-test', "data-backdrop": false })
-        div
-          .on("shown.bs.modal", function () {
-            ok($('#modal-test').is(":visible"), 'modal visible')
-            div.modal("hide")
-          })
-          .on("hidden.bs.modal", function() {
-            ok(!$('#modal-test').is(":visible"), 'modal hidden')
-            div.remove()
-            start()
-          })
-          .modal("show")
-      })
-
-      test("should close modal when clicking outside of modal-content", function () {
-        stop()
-        $.support.transition = false
-        var div = $("<div id='modal-test'><div class='contents'></div></div>")
-        div
-          .bind("shown.bs.modal", function () {
-            ok($('#modal-test').length, 'modal insterted into dom')
-            $('.contents').click()
-            ok($('#modal-test').is(":visible"), 'modal visible')
-            $('#modal-test').click()
-          })
-          .bind("hidden.bs.modal", function() {
-            ok(!$('#modal-test').is(":visible"), 'modal hidden')
-            div.remove()
-            start()
-          })
-          .modal("show")
-      })
-})
+}(window.jQuery);
