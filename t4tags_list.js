567ce27fa33fb6f91cdcca0d6d7780ffb3efd284
david viramontes <dviramontes@gmail.com>
2013-08-10 11:09:40 -0600

    adds grunt-css-min

--- t4tags_list.js
+++ t4tags_list.js
@@ -3,7 +3,7 @@
 	{ 
 		'<link rel="stylesheet" href="./css/vendor/bootstrap.css">' : '<t4 type="media" id="60197"/>'
 	}, 
-	{
+  {
 		'<link rel="stylesheet" href="./css/vendor/bootstrap.css">' : '<t4 type="media" id="60197"/>'
 	}
 ]
