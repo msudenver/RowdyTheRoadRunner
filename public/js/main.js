ffa5cf51f1d8fd7ee76a68170af5f3a2c1f0ff6c
david viramontes <dviramontes@gmail.com>
2013-08-10 17:38:18 -0600

    adds google analytics code and homepage.build

--- public/js/main.js
+++ public/js/main.js
@@ -37,8 +37,19 @@
       return console.log("resize::" + $(window).width());
     });
     if (document.location.href === "http://localhost:3000/" || document.location.href === "http://msudenver.edu/") {
-      return $('div#top3').load("/newsroom/top3/");
+      $('div#top3').load("/newsroom/top3/");
     }
+    return 
+		var _gaq = _gaq || []; 
+		_gaq.push(['_setAccount', 'UA-505529-1']); 
+		_gaq.push(['_trackPageview']); 
+
+		(function() { 
+		var ga = document.createElement('script'); ga.type = 'text/javascript'; ga.async = true; 
+		ga.src = ('https:' == document.location.protocol ? 'https://ssl' : 'http://www') + '.google-analytics.com/ga.js'; 
+		var s = document.getElementsByTagName('script')[0]; s.parentNode.insertBefore(ga, s); 
+		})(); 
+	;
   });
 
 }).call(this);
