5e8360b2c6f22076a9da02b3809094ca40134e0f
david viramontes <dviramontes@gmail.com>
2013-08-29 17:55:22 -0600

    begins slider upgrade to bootstrap 3

--- sm_build/js/utils/modernizrLoadFunction.js
+++ sm_build/js/utils/modernizrLoadFunction.js
@@ -6,13 +6,31 @@
 // });
 
 // 08.29.13 @ 10:40pm
-Modernizr.load([
-	{
-		load : '<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>',
+
+Modernizr.load([{
+		load : "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js",
 		complete : function(){
-			if(!window.jquery){Modernizr.load("<t4 type='media' id='60221' formatter='image/path'/>")}
-		},
+			// if(!window.jquery){Modernizr.load("<t4 type='media' id='60221' formatter='image/path'/>")}
+			if(!window.jQuery){ Modernizr.load("../js/vendor/jquery.min.js") } 
+		}
 		// will wait and for the fallback to load and execute..
-		load: console.log("Fallback executed, jQuery loaded from media library")
-	}
-]);
+		// ,
+		// load: console.log("Fallback executed, jQuery loaded from media library")
+}]);
+
+
+// Modernizr.load([
+//   {
+//     load: '//ajax.googleapis.com/ajax/libs/jquery/1.7.1/jquery.js',
+//     complete: function () {
+//       if ( !window.jQuery ) {
+//             Modernizr.load('js/libs/jquery-1.7.1.min.js');
+//       }
+//     }
+//   },
+//   {
+//     // This will wait for the fallback to load and
+//     // execute if it needs to.
+//     load: 'needs-jQuery.js'
+//   }
+// ]);
