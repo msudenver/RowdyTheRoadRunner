729252098d506fe181d7fcfdbb6b7327499ec3a8
david viramontes <dviramontes@gmail.com>
2013-08-29 11:44:53 -0600

    adds slider.js for carousel controls, removes normalize.css from all templates

--- sm_build/js/utils/modernizrLoadFunction.js
+++ sm_build/js/utils/modernizrLoadFunction.js
@@ -5,3 +5,14 @@
 // 	nope : ["css/utils/mediaAllForIE8.css","http://cdnjs.cloudflare.com/ajax/libs/respond.js/1.2.0/respond.min.js"]
 // });
 
+// 08.29.13 @ 10:40pm
+Modernizr.load([
+	{
+		load : '<script src="//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.min.js"></script>',
+		complete : function(){
+			if(!window.jquery){Modernizr.load("<t4 type='media' id='60221' formatter='image/path'/>")}
+		},
+		// will wait and for the fallback to load and execute..
+		load: console.log("Fallback executed, jQuery loaded from media library")
+	}
+]);
