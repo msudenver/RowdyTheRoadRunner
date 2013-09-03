a09e6622ea97162479c0055ab799e94982d838b2
david viramontes <dviramontes@gmail.com>
2013-09-03 16:31:38 -0600

    begins newsroom/carousel slider rewrite

--- sm_build/js/utils/slider.js
+++ sm_build/js/utils/slider.js
@@ -17,9 +17,47 @@
 	08.29.13
  */
 
+// original url for newsroom top 3
 // var url = "/newsroom/channels/slider/";
 
+var carousel_indicators = ['<ol class="carousel-indicators">',
+    '<li data-target="#carousel-example-generic" data-slide-to="0" class="active">',
+    '</li>',
+    '<li data-target="#carousel-example-generic" data-slide-to="1">',
+    '</li>',
+    '<li data-target="#carousel-example-generic" data-slide-to="2">',
+    '</li>',
+    '</ol>'
+].join(' ');
+
+var carousel_controls = [
+    '<a class="left carousel-control" href="#carousel-example-generic" data-slide="prev">',
+    '<span class="icon-prev"></span>',
+    '</a>',
+    '<a class="right carousel-control" href="#carousel-example-generic" data-slide="next">',
+    '<span class="icon-next"></span>',
+    '</a>'
+].join(' ');
+
 function initCarousel(){
+
+
+    $('.content').prepend('<div class="carousel slide"></div>');
+
+    carousel_div = $('.carousel');
+    carousel_div.append(carousel_indicators);
+    carousel_div.append('<div class="carousel-inner"></div>');
+    carousel_div.append(carousel_controls);
+
+    // start carousel function call
+    carousel_div.carousel();
+
+
+    // $(".carousel-inner").load(url, function(){
+    //     $(".item :first").addClass('active');
+    // });
+
+
     // $("#slider").css({
     //     "width":"100%",
     //     "margin-left": "0px",
@@ -28,24 +66,24 @@ function initCarousel(){
     //     "border" : "none"
     // })
 
-    var container= '<div class="container"><div id="slider" class="carousel slide"><div class="carousel-inner"></div><a class="carousel-control left" href="#slider" data-slide="prev">&lsaquo;</a><a class="carousel-control right" href="#slider" data-slide="next">&rsaquo;</a></div></div>';
-    var url = "slider/slider.html";
+    // var container= '<div class="container"><div id="slider" class="carousel slide"><div class="carousel-inner"></div><a class="carousel-control left" href="#slider" data-slide="prev">&lsaquo;</a><a class="carousel-control right" href="#slider" data-slide="next">&rsaquo;</a></div></div>';
+    // var url = "slider/slider.html";
 
-    $('.content').prepend('<div id="slider"></div>');
+    // $('.content').prepend('<div id="slider"></div>');
+
+    // $("#slider").html(container);
+
+    // $(".carousel-inner").load(url, function(){
+    //     $(".item :first").addClass('active');
+    // });
+
+    // $('.carousel').carousel({
+    //     interval: 5000,
+    //     pause: "hover"
+    // });
+    // // // // ! redundant  ie 10 fix
+    // $(".item :first").addClass('active');
+    // console.warn("!!! carousel function called !!!");
 
 
-
-    $("#slider").html(container);
-
-    $(".carousel-inner").load(url, function(){
-        $(".item :first").addClass('active');
-    });
-
-    $('.carousel').carousel({
-        interval: 5000,
-        pause: "hover"
-    });
-    // // ! redundant  ie 10 fix
-    $(".item :first").addClass('active');
-    console.warn("!!! carousel function called !!!");
 }
