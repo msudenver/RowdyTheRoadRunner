1e3cc4bbc5e4cdb193381fb30c120ff287905413
david viramontes <dviramontes@gmail.com>
2013-09-08 15:10:00 -0600

    renames modernizr.custom  modernizr.js

--- sm_build/js/utils/yepnope.js
+++ sm_build/js/utils/yepnope.js
@@ -10,33 +10,32 @@
 //  utils
 var log  = function(args){ console.log(args)};
 var warn = function(args){ console.warn(args)};
-var nope = function(){ return};
+var nope = function(){ return };
 
 yepnope([
+
     {
         load : "//cdnjs.cloudflare.com/ajax/libs/jquery/2.0.3/jquery.js",
         callback : function (url, result, key){
             if(!window.jQuery){
                 // load jquery.min.js, resource # 62496
-                yepnope("<t4 type='media' id='62496'/>");
-                // yepnope('../js/vendor/jquery.min.js');
+                // yepnope("<t4 type='media' id='62496'/>");
+                yepnope('../js/vendor/jquery.min.js');
                 console.warn("jQuery was loaded from Server");
-            }else {
+            } else {
                 console.warn("jQuery was loaded from CDN");
             }
         }
     },
     {
         load : "//netdna.bootstrapcdn.com/bootstrap/3.0.0-rc1/css/bootstrap.min.css",
-
-
         // callback will always fire after an error or a timeout
         // typically 5000 ms
         callback : function(url, result, key){
             if($("#bootstrapCssTest").is(":visible") === true){
                 // load bootstrap.min.css, resource # 62542
-                yepnope("<t4 type='media' id='62542'/>");
-                // yepnope('../css/vendor/bootstrap.min.css');
+                // yepnope("<t4 type='media' id='62542'/>");
+                yepnope('../css/vendor/bootstrap.min.css');
                 console.warn("bootstrap.min.css loaded from server")
             }else{
                 console.warn("bootstrap.min.css loaded from CDN")
@@ -47,8 +46,8 @@ yepnope([
     },
     {
         // load main.min.css, resource # 62508
-        // load: '../css/main.min.css',
-        load: "<t4 type='media' id='62508'/>",
+        load: '../css/main.min.css',
+        // load: "<t4 type='media' id='62508'/>",
         complete : function(){
             console.warn("main.min.css loaded from server");
         }
@@ -58,23 +57,23 @@ yepnope([
         callback : function (url, result, key){
             if(!$.fn.modal){
                 // load main.min.css, resource # 62497
-                yepnope("<t4 type='media' id='62497'/>");
-                // yepnope('../js/vendor/bootstrap/min/bootstrap.min.js');
+                // yepnope("<t4 type='media' id='62497'/>");
+                yepnope('../js/vendor/bootstrap/min/bootstrap.min.js');
                 console.warn("bootstrapmin.js was loaded from Server");
             }else{
                 console.warn("bootstrap.min.js was loaded from CDN");
             }
         },
-        complete: function(){
+        complete : function(){
             yepnope([
                 "http://www.trumba.com/scripts/spuds.js",
                 // plugins.js, resource # 62499
-                "<t4 type='media' id='62497'/>",
-                // "../js/vendor/plugins.js",
+                // "<t4 type='media' id='62497'/>",
+                "../js/vendor/plugins.js",
 
                 // main.min.js resource # 62498
-                "<t4 type='media' id='62497'/>"
-                // "../js/main.min.js"
+                // "<t4 type='media' id='62497'/>"
+                "../js/main.min.js"
             ]);
         }
     },
