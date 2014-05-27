91b18e109d9f0826982e5c6d71ff6e193d8eb297
Miguel Garza-Wicker <garzawi@msudenver.edu>
2014-05-27 12:07:03 -0600

    Changes HTML build version from 0.0.2 to 0.2.1

--- tasks/htmlbuild.js
+++ tasks/htmlbuild.js
@@ -3,7 +3,7 @@ module.exports = function(grunt) {
     'use strict';
 
     // grunt.loadNpmTasks('grunt-html-build');
-    var version = "0.0.2";
+    var version = "0.2.1";
 
     grunt.config('htmlbuild', {
 
